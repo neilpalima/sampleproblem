223e19b874d77f90a62e194bcc0b8165a63e2cf3
Neil Palima <neiljan.palima@gmail.com>
2018-05-08 20:26:50 +0800

    improve code

--- solution.js
+++ solution.js
@@ -1,66 +1,47 @@
 
-function isSpliceJoin(s, l, v) {
-    console.log(s,l,v)
+const isSpliceJoin = (s, l, v) => {
     if (s.length !== v.length) return false;
 
-    function getSpliceJoinStrings(str, length) {
-        var spliceJoinStrings = [];
+    const getSpliceJoinStrings = (str, length) => {
+        const spliceJoinStrings = [];
 
-        for (var i = 0; i < str.length; i += length) {
-            if (i+length >= str.length) {
-                spliceJoinStrings.push(str.substring(i, str.length));
-            }
-            else {
-                spliceJoinStrings.push(str.substring(i, i+length));
-            }
+        for (let i = 0; i < str.length; i += length) {
+          spliceJoinStrings.push(str.substring(i, i + length));
         }
 
         return spliceJoinStrings;
     }
 
-    var spliceStrings = getSpliceJoinStrings(s, parseInt(l));
+    const spliceStrings = getSpliceJoinStrings(s, parseInt(l));
 
-    function getSpliceJoinPermutationStrings(spliceJoinStrings, start, lastStr) {
-        var stringPermutation = [];
+    const getSpliceJoinPermutationStrings = (spliceJoinStrings, start, lastStr) => {
+        const stringPermutation = [];
 
-        function permute(arr, memo) {
-            var cur, memo = memo || [];
+        const permute = (arr, memo = []) => {
+            let cur;
 
-            for (var i = 0; i < arr.length; i++) {
+            for (let i = 0; i < arr.length; i++) {
                 if (memo.length === 0 && (arr[i] !== start && arr[i] !== lastStr)) continue;
 
                 cur = arr.splice(i, 1);
                 if (arr.length === 0) {
                   stringPermutation.push(memo.concat(cur));
                 }
-                permute(arr.slice(), memo.concat(cur));
+                permute(arr, memo.concat(cur));
                 arr.splice(i, 0, cur[0]);
             }
 
             return stringPermutation;
         }
 
-        var permutatedArr = permute(spliceJoinStrings);
-
-        var permutatedStringArr = [];
-
-        permutatedArr.forEach(function(strArr) {
-            permutatedStringArr.push(strArr.join(''));
-        });
-       return permutatedStringArr;
+       return permute(spliceJoinStrings).map(str => str.join(''));
     }
 
-    var permutatedSpliceJoinStrings = getSpliceJoinPermutationStrings(spliceStrings, v.substring(0, l), spliceStrings[spliceStrings.length-1]);
+    const permutatedSpliceJoinStrings = getSpliceJoinPermutationStrings(spliceStrings, v.substring(0, l), spliceStrings[spliceStrings.length - 1]);
 
-    for (var i = 0; i < permutatedSpliceJoinStrings.length; i++) {
-        if (permutatedSpliceJoinStrings[i].toLowerCase() === v.toLowerCase()) return true;
-    }
-
-    return false;
+    return permutatedSpliceJoinStrings.indexOf(v) !== -1;
 }
 
-var args = process.argv.slice(2);
-console.log(args)
+const args = process.argv.slice(2);
 
 console.log(isSpliceJoin(args[0], args[1], args[2]));
-// console.log(isSpliceJoin("abacaaa", 2, "aaacaba"));
